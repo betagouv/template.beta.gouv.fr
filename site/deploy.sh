1b0f8f1ef32fa29d283dd7b2ff031bb3ada5b0e5
thimy <thimy@codeursenliberte.fr>
2020-02-26 15:26:06 +0100

    change deploy to push

--- site/deploy.sh
+++ site/deploy.sh
@@ -25,6 +25,5 @@ hugo
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-#echo "Pushing to github"
-#git push --all
-
+echo "Pushing to github"
+git push --all
